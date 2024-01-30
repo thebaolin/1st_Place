8ad23b10d3f3c9cf195743654fac6f0f9bb963f9
MacKoltovs <104460855+MacKoltovs@users.noreply.github.com>
2024-01-29 23:27:41 -0500

    Round Tracking and Randomization

--- web.py
+++ web.py
@@ -1,15 +1,17 @@
 from flask import Flask, redirect, url_for, render_template, request
+import random
 
 app = Flask(__name__)
 
 def initialize_lists():
-    global restaurants, choices
+    global restaurants, choices, round_num
     restaurants = ["A", "B", "C", "D"]
     choices = []
+    round_num = 1
 
 
 
-@app.route("/")
+@app.route("/", methods=['GET' , 'POST'])
 def home():
     return render_template("index.html")
 
@@ -20,10 +22,11 @@ def begin():
 
 @app.route("/choose", methods=['GET','POST'])
 def choose():
-    global restaurants, choices
+    global restaurants, choices , round_num
     if request.method == 'GET':
+        random.shuffle(restaurants)
         curr = restaurants[:2]
-        return render_template("restaurants.html" , given = curr)
+        return render_template("restaurants.html" , given = curr , rounds = round_num)
     elif request.method == 'POST':
         restaurant_choice = request.form.get("restaurant")
 
@@ -34,10 +37,11 @@ def choose():
 
         if restaurants:
             curr = restaurants[:2]
-            return render_template("restaurants.html" , given = curr)
+            return render_template("restaurants.html" , given = curr, rounds = round_num)
         elif len(choices) > 1:
             restaurants = choices
             choices = []
+            round_num += 1
             return redirect(url_for('choose'))
         else:
             return redirect(url_for('result'))
