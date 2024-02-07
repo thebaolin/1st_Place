cf9d53e1b307ca367ad6a6f4fb8514742e6262d0
Max Koltovskiy <mkoltovskiy@gmail.com>
2024-02-06 22:57:01 -0500

    Backend

--- location.py
+++ location.py
@@ -1,15 +1,26 @@
 import config
 
 class Location:
-    def __init__(self, name, address, rating, totalRating, type, photos):
+    def __init__(self, name, address, rating, totalRating, type, restaurantPic, photos, price):
         self.name = name
         self.address = address
         self.rating = rating
         self.totalRating = totalRating
+        self.restaurantPic = restaurantPic
+
         self.photos = self.createURL(photos)
+
         words = type.split('_')
         self.type = words[0].capitalize()
-        print(self.photos)
+
+        if price == "PRICE_LEVEL_INEXPENSIVE ":
+            self.price = "$"
+        elif price == "PRICE_LEVEL_MODERATE":
+            self.price = "$$"
+        elif price == "PRICE_LEVEL_EXPENSE":
+            self.price = "$$$"
+        else:
+            self.price = "$$$$"
 
     def createURL(self, photos):
         tmp = []
--- server.py
+++ server.py
@@ -1,25 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import config
+import random
 from location import Location
 
 app = Flask(__name__)
 api_key = config.API_KEY
-list = []
+
+def initialize_lists():
+    global list, choices, round_num
+    list = []
+    choices = []
+    round_num = 1
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     if request.method == "POST":
+        initialize_lists()
         input_value = request.form.get('location')
         latitude, longitude = geocode(input_value)
         data = getJSON(latitude, longitude)
         locations = data['places']
         for place in locations:
             pics = []
-            for pic in place['photos']:
+            for pic in place['photos'][1:]:
                 pics.append(pic['name'])
-            tmp = Location(place['displayName']['text'], place['formattedAddress'], place['rating'], place['userRatingCount'], place['primaryType'], pics)
+            tmp = Location(place['displayName']['text'], place['formattedAddress'], place['rating'], place['userRatingCount'], place['primaryType'], f"https://places.googleapis.com/v1/{place['photos'][0]['name']}/media?maxHeightPx=400&maxWidthPx=400&key={api_key}", pics, place['priceLevel'])
             list.append(tmp)
+        return redirect(url_for('choose'))
 
     return render_template("index.html", api_key = api_key)
 
@@ -35,7 +43,7 @@ def getJSON(latitude, longitude):
     URL = "https://places.googleapis.com/v1/places:searchNearby"
     payload = {
         'includedTypes' : ['japanese_restaurant'],
-        'maxResultCount' : 2,
+        'maxResultCount' : 20,
         'locationRestriction' : {
             "circle": {
                 "center": {
@@ -50,7 +58,7 @@ def getJSON(latitude, longitude):
     headers = {
         'Content-Type': 'application/json',
         'X-Goog-Api-Key': api_key,
-        'X-Goog-FieldMask': 'places.rating,places.userRatingCount,places.displayName,places.formattedAddress,places.primaryType,places.photos'
+        'X-Goog-FieldMask': 'places.rating,places.userRatingCount,places.displayName,places.formattedAddress,places.primaryType,places.photos,places.priceLevel'
     }
 
     response = requests.post(URL, json=payload, headers=headers)
@@ -74,6 +82,39 @@ def geocode(address):
     else:
         print(f"Failed to geocode address. Status code: {response.status_code}")
 
+@app.route("/choose", methods=['GET','POST'])
+def choose():
+    global list, choices, round_num
+    if request.method == 'GET':
+        random.shuffle(list)
+        curr = list[:2]
+        return render_template("restaurants.html" , given = curr , rounds = round_num)
+    elif request.method == 'POST':
+        restaurant_choice = request.form.get("restaurant")
+
+        choices.append(restaurant_choice)
+
+        list.pop(0)
+        list.pop(0)
+
+        if len(list) == 1:
+            choices.append(list[0])
+            list.pop(0)
+
+        if list:
+            curr = list[:2]
+            return render_template("restaurants.html" , given = curr, rounds = round_num)
+        elif len(choices) > 1:
+            list = choices
+            choices = []
+            round_num += 1
+            return redirect(url_for('choose'))
+        else:
+            return redirect(url_for('result'))
+
+@app.route("/result")
+def result():
+    return render_template("result.html", choice=choices[0])
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8000)
